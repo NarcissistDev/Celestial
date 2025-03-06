02473af57f70ca8a0bba1dc5b235be5023811290
Narcissist <galaxiyt07@gmail.com>
2025-03-06 23:22:12 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,24 +5,6 @@ if getgenv().MainKey == "nil" then
     --// Game Checker
     if game.PlaceId == 142823291 then -- MM2 Normal & Disguises
       
-        repeat wait() until pcall(function() return game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.Game.Crafting.Inventory.Salvage end)
-
-         spawn(function() 
-    while true do
-        pcall(function()
-            local gui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("TradeGUI")
-            if gui then
-                gui.Enabled = false
-                local frame = gui:FindFirstChild("Container")
-                if frame then
-                    frame.Visible = false
-                end
-            end
-        end)
-        wait()
-    end
-end)
-      
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NarcissistDev/Resources/refs/heads/main/TestEnvironment"))()
       
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NarcissistDev/Celestial/main/MM2"))() --# Main Load
